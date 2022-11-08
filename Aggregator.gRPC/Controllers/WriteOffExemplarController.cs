4b545b8d3a3ca124d93010d6e766647e711f6e91
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-11-08 22:42:36 +0200

    gRPC for deleting exemplar

---
+++ Aggregator.gRPC/Controllers/WriteOffExemplarController.cs
@@ -0,0 +1,28 @@
+﻿using Aggregator.gRPC.Services;
+using Microsoft.AspNetCore.Mvc;
+using WrittenOffManagement.API.Protos;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Aggregator.gRPC.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WriteOffExemplarController : ControllerBase
+    {
+        ExemplarAggregatorService exemplarAggregatorService;
+
+        public WriteOffExemplarController(ExemplarAggregatorService exemplarAggregatorService)
+        {
+            this.exemplarAggregatorService = exemplarAggregatorService;
+        }
+
+
+        // DELETE api/<WriteOffExemplarController>/5
+        [HttpDelete("{id}")]
+        public async Task<WrittenOffModel> Delete(long id)
+        {
+            return await exemplarAggregatorService.writeOfExemplar(id);
+        }
+    }
+}
---
+++ Catalog.API/Services/LiteratureGrpcService.cs
@@ -0,0 +1,66 @@
+﻿using AutoMapper;
+using Catalog.API.Protos;
+using Catalog.DAL.UOW.Interface;
+using Grpc.Core;
+using static Catalog.API.Protos.Literature;
+
+namespace Catalog.API.Services
+{
+    public class LiteratureGrpcService : LiteratureBase
+    {
+        ILogger<LiteratureGrpcService> logger;
+        IMapper mapper;
+        IUnitOfWork unitOfWork;
+
+
+        public LiteratureGrpcService(ILogger<LiteratureGrpcService> logger, IUnitOfWork unitOfWork)
+        {
+            this.logger = logger;
+            this.unitOfWork = unitOfWork;
+           
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.CreateMap<DAL.Entity.Literature, LiteratureModel>()
+                .ForMember(model => model.AuthorName,
+                    conf => conf.MapFrom(entity => entity.Author.Name))
+                .ForMember(model => model.AuthorDescription,
+                    conf => conf.MapFrom(entity => entity.Author.Description))
+                .ForMember(model => model.PublisherName,
+                    conf => conf.MapFrom(entity => entity.Publisher.Name))
+                .ForMember(model => model.PublisherDescription,
+                    conf => conf.MapFrom(entity => entity.Publisher.Description))
+                .ForMember(model => model.CreatedAt,
+                    conf => conf.MapFrom(
+                        entity => Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.SpecifyKind(entity.CreatedAt?? DateTime.Now, DateTimeKind.Utc))))
+                .ForMember(model => model.UpdatedAt,
+                    conf => conf.MapFrom(
+                        entity => Google.Protobuf.WellKnownTypes.Timestamp.FromDateTime(DateTime.SpecifyKind(entity.UpdatedAt ?? DateTime.Now, DateTimeKind.Utc))));
+
+            });
+            mapper = new Mapper(config);
+        }
+
+        public override async Task GetAllLiterature(GetAllLiteratureRequest request, IServerStreamWriter<LiteratureModel> responseStream, ServerCallContext context)
+        {
+            var allLiterature = await unitOfWork.LiteratureRepository.GetAsync();
+            foreach (var literature in allLiterature)
+            {
+                await responseStream.WriteAsync(mapper.Map<LiteratureModel>(literature));
+            }
+            logger.LogInformation($"gRPC returned all literature");
+        }
+
+        public override async Task<LiteratureModel> GetLiteratureById(GetLiteratureByIdRequest request, ServerCallContext context)
+        {
+            var toReturn = await unitOfWork.LiteratureRepository.GetCompleteEntityAsync(request.LiteratureId);
+            if (toReturn == null)
+            {
+                logger.LogError($"gRPC literature with id={request.LiteratureId} not found");
+                throw new RpcException(
+                    new Status(StatusCode.NotFound, $"Literature with id={request.LiteratureId} not found"));
+            }
+            logger.LogInformation($"gRPC returned literature with id={request.LiteratureId}");
+            return mapper.Map<LiteratureModel>(toReturn);
+        }
+    }
+}
