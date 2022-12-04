1389873f4925b0207d769fbc44d533e054566b08
ArtemBabaiev <90097902+ArtemBabaiev@users.noreply.github.com>
2022-12-04 15:07:43 +0200

    aggregator logging

--- Agregator/Program.cs
+++ Agregator/Program.cs
@@ -3,6 +3,8 @@ using Agregator.Services.Interfaces;
 using Polly;
 using Polly.Extensions.Http;
 using Serilog;
+using Serilog.Sinks.Elasticsearch;
+using System.Reflection;
 
 internal class Program
 {
@@ -30,6 +32,22 @@ internal class Program
                 c.BaseAddress = new Uri(builder.Configuration["ApiSettings:RecordUrl"]))
             .AddPolicyHandler(GetRetryPolicy())
             .AddPolicyHandler(GetCircuitBreakerPolicy()); ;
+
+        builder.Host.UseSerilog((context, configuration) =>
+        {
+            configuration.Enrich.FromLogContext()
+            .Enrich.WithMachineName()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(new ElasticsearchSinkOptions(new Uri(context.Configuration["ElasticConfiguration:Uri"]))
+            {
+                IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-logs-{DateTime.UtcNow:yyyy-MM}",
+                AutoRegisterTemplate = true,
+                NumberOfShards = 2,
+                NumberOfReplicas = 1
+            })
+            .Enrich.WithProperty("Enviroment", context.HostingEnvironment.EnvironmentName)
+            .ReadFrom.Configuration(context.Configuration);
+        });
 
         var app = builder.Build();
 
